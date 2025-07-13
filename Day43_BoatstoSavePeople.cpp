988c6d52ef6664e526648b9aa1524b8800353605
Prince Verma <149296214+Pverma-1234@users.noreply.github.com>
2025-07-13 20:48:58 +0530

    added

--- Day43_BoatstoSavePeople.cpp
+++ Day43_BoatstoSavePeople.cpp
@@ -1,4 +1,4 @@
-42. Boats to Save People
+43. Boats to Save People
 
 You are given an array people where people[i] is the weight of the ith person, and an infinite number of boats where each boat can carry a maximum weight of limit. Each boat carries at most two people at the same time, provided the sum of the weight of those people is at most limit.
 
