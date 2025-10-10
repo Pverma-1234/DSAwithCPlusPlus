08d2f43ecccb8063b16c7109d912e8669fdb2563
Prince Verma <149296214+Pverma-1234@users.noreply.github.com>
2025-10-10 23:29:57 +0530

    added

--- Day69_RemoveDuplicatesfromSortedList.cpp
+++ Day69_RemoveDuplicatesfromSortedList.cpp
@@ -1,4 +1,4 @@
-68. Remove Duplicates from Sorted List
+69. Remove Duplicates from Sorted List
 
 Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
 
