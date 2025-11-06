4d82a94227bfc5b6bae341d3dfb1b68093dc9856
Prince Verma <149296214+Pverma-1234@users.noreply.github.com>
2025-11-06 23:00:03 +0530

    added

---
+++ Day108_3Sum Closest.cpp
@@ -0,0 +1,60 @@
+108. 3Sum Closest
+
+Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target.
+
+Return the sum of the three integers.
+
+You may assume that each input would have exactly one solution.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,2,1,-4], target = 1
+Output: 2
+Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+Example 2:
+
+Input: nums = [0,0,0], target = 1
+Output: 0
+Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
+ 
+
+Constraints:
+
+3 <= nums.length <= 500
+-1000 <= nums[i] <= 1000
+-104 <= target <= 104
+
+</>CODE:
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+
+        int closestSum = nums[0] + nums[1] + nums[2]; 
+
+        for (int i = 0; i < n - 2; ++i) {
+            int left = i + 1, right = n - 1;
+
+            while (left < right) {
+                int currSum = nums[i] + nums[left] + nums[right];
+
+                if (abs(currSum - target) < abs(closestSum - target)) {
+                    closestSum = currSum;
+                }
+
+                if (currSum == target) {
+                    return target; 
+                } else if (currSum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return closestSum;
+    }
+};
