4ca0aa8e959daf0ffb871561f58cb80d6e5147c9
Prince Verma <149296214+Pverma-1234@users.noreply.github.com>
2025-11-17 22:31:10 +0530

    added

---
+++ Day120_Check If All 1's Are at Least Length K Places Away.cpp
@@ -0,0 +1,42 @@
+120. Check If All 1's Are at Least Length K Places Away
+
+Given an binary array nums and an integer k, return true if all 1's are at least k places away from each other, otherwise return false.
+
+ 
+
+Example 1:
+
+
+Input: nums = [1,0,0,0,1,0,0,1], k = 2
+Output: true
+Explanation: Each of the 1s are at least 2 places away from each other.
+Example 2:
+
+
+Input: nums = [1,0,0,1,0,1], k = 2
+Output: false
+Explanation: The second 1 and third 1 are only one apart from each other.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+0 <= k <= nums.length
+nums[i] is 0 or 1
+
+</>CODE:
+class Solution {
+public:
+    bool kLengthApart(vector<int>& nums, int k) {
+        int prev = -1;  
+        
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 1) {
+                if (prev != -1 && i - prev - 1 < k)
+                    return false;
+                prev = i;
+            }
+        }
+        return true;
+    }
+};
