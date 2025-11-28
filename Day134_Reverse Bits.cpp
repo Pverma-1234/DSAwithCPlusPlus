96d562692fbb0c3ecff117ae300f225fa5dea07f
Prince Verma <149296214+Pverma-1234@users.noreply.github.com>
2025-11-28 23:18:32 +0530

    added

---
+++ Day134_Reverse Bits.cpp
@@ -0,0 +1,46 @@
+134. Reverse Bits
+Reverse bits of a given 32 bits signed integer.
+
+ 
+Example 1:
+
+Input: n = 43261596
+
+Output: 964176192
+
+Explanation:
+
+Integer	Binary
+43261596	00000010100101000001111010011100
+964176192	00111001011110000010100101000000
+Example 2:
+
+Input: n = 2147483644
+
+Output: 1073741822
+
+Explanation:
+
+Integer	Binary
+2147483644	01111111111111111111111111111100
+1073741822	00111111111111111111111111111110
+ 
+
+Constraints:
+
+0 <= n <= 231 - 2
+n is even.
+
+</>CODE:
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t result = 0;
+        for (int i = 0; i < 32; i++) {
+            result <<= 1;         
+            result |= (n & 1);    
+            n >>= 1;              
+        }
+        return result;
+    }
+};
