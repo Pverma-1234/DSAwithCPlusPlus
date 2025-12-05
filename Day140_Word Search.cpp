20247813c01ccba6baf7bba8cf9dd54202858b60
Prince Verma <149296214+Pverma-1234@users.noreply.github.com>
2025-12-05 09:37:54 +0530

    added

---
+++ Day140_Word Search.cpp
@@ -0,0 +1,62 @@
+140. Word Search
+
+Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+
+The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+ 
+
+Example 1:
+
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+Output: true
+Example 2:
+
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
+Output: true
+Example 3:
+
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+Output: false
+ 
+
+Constraints:
+
+m == board.length
+n = board[i].length
+1 <= m, n <= 6
+1 <= word.length <= 15
+board and word consists of only lowercase and uppercase English letters.
+
+</>CODE:
+class Solution {
+public:
+    bool dfs(vector<vector<char>>& board,string word,int i,int j,int k){
+        int m=board.size(),n=board[0].size();
+        if(k==word.length())return true;
+        if(i<0 || j<0 || i>=m || j>=n || board[i][j]!=word[k]) return false;
+        char temp=board[i][j];
+        board[i][j]='$';
+        bool found=dfs(board,word,i+1,j,k+1) ||
+                   dfs(board,word,i-1,j,k+1) ||
+                   dfs(board,word,i,j+1,k+1) ||
+                   dfs(board,word,i,j-1,k+1);
+        board[i][j]=temp;
+        return found;
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        int m=board.size(),n =board[0].size();
+        if(word.length()> m*n) return false;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]==word[0] &&dfs(board,word,i,j,0)){
+                    return true;
+                }
+            }
+        }
+        return false; 
+    }
+};
