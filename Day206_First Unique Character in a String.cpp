8c72be9b9c86a3d4e743c02271c9b86ed61f55e8
Prince Verma <149296214+Pverma-1234@users.noreply.github.com>
2026-04-23 01:15:58 +0530

    added

---
+++ Day206_First Unique Character in a String.cpp
@@ -0,0 +1,58 @@
+206. First Unique Character in a String
+
+Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+ 
+
+Example 1:
+
+Input: s = "leetcode"
+
+Output: 0
+
+Explanation:
+
+The character 'l' at index 0 is the first character that does not occur at any other index.
+
+Example 2:
+
+Input: s = "loveleetcode"
+
+Output: 2
+
+Example 3:
+
+Input: s = "aabb"
+
+Output: -1
+
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of only lowercase English letters.
+
+</>CODE:
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char,int> m;
+        queue<int> Q;
+
+        for(int i=0;i<s.size();i++){
+            if(m.find(s[i]) == m.end()){
+                Q.push(i);
+            }
+            m[s[i]]++;
+
+        
+            while(Q.size() > 0 && m[s[Q.front()]] > 1){
+                Q.pop();
+            }
+
+        }
+        return Q.empty() ? -1:Q.front();
+        
+    }
+};
