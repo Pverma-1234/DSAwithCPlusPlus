f3dd6ab402c59e12021866cbd8aaa8f4157392d5
Prince Verma <149296214+Pverma-1234@users.noreply.github.com>
2026-08-08 22:28:35 +0530

    added

---
+++ Day225_Union of 2 Sorted Arrays.cpp
@@ -0,0 +1,76 @@
+225.Union of 2 Sorted Arrays
+
+Given two sorted arrays a[] and b[], where each array may contain duplicate elements , the task is to return the elements in 
+the union of the two arrays in sorted order. Union of two arrays can be defined as the set containing distinct common elements
+that are present in either of the arrays.
+
+Examples:
+
+Input: a[] = [1, 2, 3, 4, 5], b[] = [1, 2, 3, 6, 7]
+Output: [1, 2, 3, 4, 5, 6, 7]
+Explanation: Distinct elements including both the arrays are: 1 2 3 4 5 6 7.
+Input: a[] = [2, 2, 3, 4, 5], b[] = [1, 1, 2, 3, 4]
+Output: [1, 2, 3, 4, 5]
+Explanation: Distinct elements including both the arrays are: 1 2 3 4 5.
+Input: a[] = [1, 1, 1, 1, 1], b[] = [2, 2, 2, 2, 2]
+Output: [1, 2]
+Explanation: Distinct elements including both the arrays are: 1 2.
+
+Constraints:
+1  ≤  a.size(), b.size()  ≤  105
+-109 ≤ a[i], b[i] ≤109
+
+</>CODE:
+class Solution {
+  public:
+    vector<int> findUnion(vector<int> &a, vector<int> &b) {
+        // code here
+        vector<int>ans;
+        int i=0;
+        int j=0;
+        while(i<a.size() && j<b.size()){
+            if(a[i]<b[j]){
+                if(ans.empty() || ans.back()!=a[i]){
+                    ans.push_back(a[i]);
+                }
+                i++;
+            }
+            else if(b[j]<a[i]){
+                if(ans.empty() || ans.back()!=b[j]){
+                    ans.push_back(b[j]);
+                }
+                j++;
+            }else{
+                // a[i]==b[j]
+                if(ans.empty() || ans.back()!=a[i]){
+                    ans.push_back(a[i]);
+                }
+                i++;
+                j++;
+            }
+        }
+        // remaining element of a
+        while(i<a.size()){
+            if (ans.empty() || ans.back()!=a[i]){
+                ans.push_back(a[i]);
+            }
+            i++;
+        }
+        // remaining element of b
+        while(j<b.size()){
+            if(ans.empty() || ans.back()!=b[j]){
+                ans.push_back(b[j]);
+                
+            }
+            j++;
+        }
+        return ans;
+    }
+};
+
+
+
+
+
+
+
